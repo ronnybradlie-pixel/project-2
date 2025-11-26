684cd9f640a1164730070ce44832bdad29b73184
ronny <ronnybradlie@gmail.com>
2025-11-26 13:17:55 +0300

    second commit, commit with all changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const filterButtons =document.querySelectorAll(".filter-btn")
+const movieCards =document.querySelectorAll(".card")
+
+filterButtons.forEach(button =>{
+    button.addEventListener("click"( =>)
+    document.querySelector(".filter-btn.active")
+)
+})
