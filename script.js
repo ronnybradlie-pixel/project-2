3fa1b9c42495cef84d71c938993a7cb65eac552a
ronny <ronnybradlie@gmail.com>
2025-11-27 13:13:22 +0300

    commit all changes

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
+// filter
 const filterButtons =document.querySelectorAll(".filter-btn")
 const movieCards =document.querySelectorAll(".card")
 
 filterButtons.forEach(button =>{
-    button.addEventListener("click"( =>)
-    document.querySelector(".filter-btn.active")
-)
-})
+ btn.addEventListener("click", () => {
+ document.querySelector(".filter-btn.active")?.classList.remove("active");
+ button.classList.add("active");
+
+   const filter = btn.dataset.filter;
+
+    categories.forEach(cat => {
+     if (filter === "all") {
+       cat.style.display = "block";
+       } else {
+      cat.style.display = 
+      cat.querySelector(".card")?.dataset.category === filter 
+       ? "block" 
+       : "none";
+           }
+        });
+    });
+});
+
+//
+
