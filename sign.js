7c838417890c9a32add273412d15c95820f15c45
ronny <ronnybradlie@gmail.com>
2025-12-10 12:47:00 +0300

    add signup script

--- sign.js
+++ sign.js
@@ -1,3 +1,27 @@
+const signupForm = document.getElementById("signupForm");
+
+// If signed up,go tologin page
+if (localStorage.getItem("user")) {
+  window.location.href = "login.html";
+}
+
+signupForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const name = document.getElementById("signupName").value;
+  const email = document.getElementById("signupEmail").value;
+  const password = document.getElementById("signupPassword").value;
+
+  const user = { name, email, password };
+
+  localStorage.setItem("user", JSON.stringify(user));
+
+  alert("Account created successfully!");
+
+  // After signing up go to login page
+  window.location.href = "login.html";
+});
+
 const loginForm = document.getElementById("loginForm");
 
 if (!localStorage.getItem("user")) {
